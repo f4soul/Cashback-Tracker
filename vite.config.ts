c399806ce6d3b9c38317effad0629077c12dba62
Anton Filimonov <135710412+f4soul@users.noreply.github.com>
2026-03-25 17:09:20 +0300

    Change base path to '/Cashback-Tracker/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/cashback-tracker/',
+  base: '/Cashback-Tracker/',
   plugins: [
     react(),
     tailwindcss(),
